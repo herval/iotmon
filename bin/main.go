4426522a27cb6411d2bd345a732e82efa49e324e
herval <hervalfreire@gmail.com>
2021-05-18 10:45:14 -0700

    webserver?

--- bin/main.go
+++ bin/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/herval/iotcollector/pkg/awair"
 	"github.com/herval/iotcollector/pkg/prom"
 	_ "github.com/joho/godotenv/autoload"
@@ -20,42 +21,62 @@ func main() {
 		os.Getenv("AWAIR_TOKEN"),
 	)
 
-	go func(upd awair.Updates) {
-		if err := awairMonitor.Start(upd, time.Minute*10); err != nil {
-			panic(err)
-		}
-	}(updates)
+	port := os.Getenv("PORT")
 
-	go func(upd awair.Updates) {
-		pushers := PusherBuffer{
-			pushers:    map[int]prom.Pusher{},
-			gatewayUrl: os.Getenv("PROMETHEUS_URL"),
-		}
+	go startWebserver(port)
 
-		for d := range upd {
-			fmt.Println("Processing metrics...")
-			pusher := pushers.For(d.DeviceId)
-			pusher.Update("score", d.Score)
-			for _, s := range d.Sensors {
-				kind := strings.ToLower(s.Comp)
-				if !pusher.Update(kind, s.Value) {
-					fmt.Println("Skipping '" + kind + "' measurement")
-				}
-			}
+	go awairPoll(awairMonitor, updates)
 
-			fmt.Println(fmt.Sprintf("%+v", d))
-			err := pusher.Push()
-			if err != nil {
-				fmt.Println(err.Error())
-				// TODO
-			} else {
-				fmt.Println("Pushed")
-			}
-		}
-
-	}(updates)
+	go promPush(updates)
 
 	select {}
+}
+
+func startWebserver(port string) {
+	r := gin.Default()
+	//r.LoadHTMLGlob("./resources/templates/*")
+
+	r.GET("/", func(c *gin.Context) {
+		c.Status(200)
+	})
+	err := r.Run(port)
+	if err != nil {
+		fmt.Println("Error starting http server - %s", err.Error())
+	}
+}
+
+func awairPoll(awairMonitor *awair.Monitor, upd awair.Updates) {
+	if err := awairMonitor.Start(upd, time.Minute*10); err != nil {
+		panic(err)
+	}
+}
+
+func promPush(upd awair.Updates) {
+	pushers := PusherBuffer{
+		pushers:    map[int]prom.Pusher{},
+		gatewayUrl: os.Getenv("PROMETHEUS_URL"),
+	}
+
+	for d := range upd {
+		fmt.Println("Processing metrics...")
+		pusher := pushers.For(d.DeviceId)
+		pusher.Update("score", d.Score)
+		for _, s := range d.Sensors {
+			kind := strings.ToLower(s.Comp)
+			if !pusher.Update(kind, s.Value) {
+				fmt.Println("Skipping '" + kind + "' measurement")
+			}
+		}
+
+		fmt.Println(fmt.Sprintf("%+v", d))
+		err := pusher.Push()
+		if err != nil {
+			fmt.Println(err.Error())
+			// TODO
+		} else {
+			fmt.Println("Pushed")
+		}
+	}
 }
 
 type PusherBuffer struct {
